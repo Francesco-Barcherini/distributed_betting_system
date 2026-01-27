b71c63014e9b2b7dfd52440dffa43ba15db51271
francesco-scar <francesco_git@protonmail.com>
2026-01-27 21:40:13 +0100

    🔒️ Fix path for my-bets.html in security configuration

--- spring/src/main/java/it/unipi/distributed_betting_system/security/SecurityConfig.java
+++ spring/src/main/java/it/unipi/distributed_betting_system/security/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
                                 "/admin.html",
                                 "/dashboard.html",
                                 "/bet-detail.html",
-                                "my-bets.html",
+                                "/my-bets.html",
                                 "/css/**",
                                 "/js/**",
                                 "/api/auth/**",
